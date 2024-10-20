42e030a0f4fba525a121ba9df7b82df3c24ecb34
DIOR <diertojiev@yandex.ru>
2024-10-20 17:57:45 +0300

    Доработанный блок 4-й с адаптацией

--- js/modules/works.js
+++ js/modules/works.js
@@ -1,31 +1,65 @@
 export default function works() {
-    document.querySelectorAll('.before-after-container').forEach(container => {
-        const beforeImage = container.getAttribute('data-before');
-        const afterImage = container.getAttribute('data-after');
-        
-        // Назначаем фоны для before и after
-        const beforeElement = container.querySelector('.before_after');
-        const afterElement = container.querySelector('figure');
-    
-        // Присваиваем background-image
-        beforeElement.style.backgroundImage = `url(${beforeImage})`;
-        afterElement.style.backgroundImage = `url(${afterImage})`;
-    });
-    
-    // Добавляем обработчики для слайдеров
-    document.querySelectorAll('.before_after_slider').forEach(slider => {
-        slider.addEventListener('input', () => {
-            beforeAfter(slider); // Вызываем локальную функцию для обработки
-        });
-    });
+    const imageComparisonSliders = document.querySelectorAll('.image-comparison__slider-wrapper');
 
-    // Локальная функция для обработки слайдера
-    function beforeAfter(slider) {
-        const container = slider.closest('.before-after-container');
-        const beforeElement = container.querySelector('.before_after');
-        
-        // Получаем значение слайдера и изменяем ширину элемента before
-        const value = slider.value;
-        beforeElement.style.width = `${value}%`;
+    function setSliderState(e, sliderRange) {
+        if (e.type === 'input') {
+            sliderRange.classList.add('image-comparison__range--active');
+            return;
+        }
+        sliderRange.classList.remove('image-comparison__range--active');
     }
+
+    function moveSliderThumb(e, sliderRange, thumb) {
+        let position = e.layerY - 20;
+
+        if (e.layerY <= sliderRange.offsetTop) {
+            position = -20;
+        }
+
+        if (e.layerY >= sliderRange.offsetHeight) {
+            position = sliderRange.offsetHeight - 20;
+        }
+
+        thumb.style.top = `${position}px`;
+    }
+
+    function moveSliderRange(e, element) {
+        const value = e.target.value;
+        const slider = element.querySelector('[data-image-comparison-slider]');
+        const imageWrapperOverlay = element.querySelector('[data-image-comparison-overlay]');
+        const thumb = element.querySelector('[data-image-comparison-thumb]');
+
+        slider.style.left = `${value}%`;
+        imageWrapperOverlay.style.width = `${value}%`;
+        moveSliderThumb(e, slider.parentElement, thumb);
+
+        setSliderState(e, element.querySelector('[data-image-comparison-range]'));
+    }
+
+    function init(element) {
+        const sliderRange = element.querySelector('[data-image-comparison-range]');
+        const thumb = element.querySelector('[data-image-comparison-thumb]');
+
+        if ('ontouchstart' in window === false) {
+            sliderRange.addEventListener('mouseup', e => setSliderState(e, sliderRange));
+
+            function moveSliderThumbHandler(e) {
+                moveSliderThumb(e, sliderRange, thumb);
+            }
+
+            sliderRange.addEventListener('mousedown', e => {
+                moveSliderThumb(e, sliderRange, thumb);
+                element.addEventListener('mousemove', moveSliderThumbHandler);
+            });
+
+            sliderRange.addEventListener('mouseup', () => {
+                element.removeEventListener('mousemove', moveSliderThumbHandler);
+            });
+        }
+
+        sliderRange.addEventListener('input', e => moveSliderRange(e, element));
+        sliderRange.addEventListener('change', e => moveSliderRange(e, element));
+    }
+
+    imageComparisonSliders.forEach(slider => init(slider));
 }
